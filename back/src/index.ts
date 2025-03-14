716514109e20d29402d9a564eab7651a74dcbec1
Lola Campos <lolacamposcipo@gmail.com>
2025-03-14 12:35:50 -0300

    Arreglos de tsconfig.json, dist y .gitignore para incluir dist

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,12 +3,13 @@ import server from "./server";
 import {DB_PORT, PORT} from "./config/envs";
 import { AppDataSource } from "./config/data-source";
 
+const portLocal = PORT||3456;
 AppDataSource.initialize()
 .then (()=> {
     console.log(`Database connected on port ${DB_PORT}`);
 
-    server.listen(PORT, () => {
-        console.log(`Server listening on PORT ${PORT}`);
+    server.listen(portLocal, () => {
+        console.log(`Server listening on PORT ${portLocal}`);
     })
 })
 .catch((error)=> console.log(error));
