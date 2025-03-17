86989585a01d17cc72ca832dc6112a0ca40432aa
Lola Campos <lolacamposcipo@gmail.com>
2025-03-17 15:13:10 -0300

    Variables de entorno y DB deploy

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,24 +1,23 @@
 import { DataSource } from "typeorm";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./envs";
+import { DB_URL, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER, DB_PASSWORD_DEPLOY, DB_NAME_DEPLOY, DB_USERNAME_DEPLOY, DB_HOST_DEPLOY, DB_PORT_DEPLOY,  } from "./envs";
 import User from "../entities/User";
 import Turno from "../entities/Turno";
 import Credentials from "../entities/Credentials";
 
-
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database:  DB_NAME,
+    host: DB_HOST_DEPLOY,
+    port: Number(DB_PORT_DEPLOY),
+    username: DB_USERNAME_DEPLOY,
+    password: DB_PASSWORD_DEPLOY,
+    database: DB_NAME_DEPLOY,
     synchronize: false,
     dropSchema: false,
     logging: ["error"],
     entities: [User, Turno, Credentials],
     subscribers: [],
     migrations: [],
-    // ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-})
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -14,3 +14,10 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = process.env.DB_NAME;
+export const DB_URL = process.env.DB_URL;
+
+export const DB_NAME_DEPLOY= process.env.DB_NAME_DEPLOY;
+export const DB_USERNAME_DEPLOY= process.env.DB_USERNAME_DEPLOY;
+export const DB_PASSWORD_DEPLOY= process.env.DB_PASSWORD_DEPLOY;
+export const DB_HOST_DEPLOY= process.env.DB_HOST_DEPLOY;
+export const DB_PORT_DEPLOY= process.env.DB_PORT_DEPLOY;
